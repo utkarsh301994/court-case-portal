5e0b94187b7521008e062f2845fbc5c3a50f2cc5
utkarsh301994 <utkarsh301994@gmail.com>
2025-06-08 22:11:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,12 +21,4 @@ fetch(API_URL)
     });
   });
 
-document.getElementById("searchInput").addEventListener("input", function () {
-  const filter = this.value.toLowerCase();
-  const rows = document.querySelectorAll("#caseTable tbody tr");
 
-  rows.forEach(row => {
-    const text = row.textContent.toLowerCase();
-    row.style.display = text.includes(filter) ? "" : "none";
-  });
-});
