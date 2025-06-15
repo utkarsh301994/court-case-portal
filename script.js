caedc9902697cc2f3c680182bbf3a6e5c163d826
utkarsh301994 <utkarsh301994@gmail.com>
2025-06-15 14:14:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 const supabaseUrl = 'https://lrllzxsiavbqdksvxlao.supabase.co'; // your URL
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxybGx6eHNpYXZicWRrc3Z4bGFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk4ODcyMDYsImV4cCI6MjA2NTQ2MzIwNn0._vP4HPl3Jl49kMXMxzgCdVIa2NAwuKUTolCMRIOATQM'; // your anon key
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
 
 
 function addLandEntry() {
@@ -18,7 +18,7 @@ function addLandEntry() {
 }
 
 async function loadCases() {
-  const { data, error } = await supabase
+  const { data, error } = await supabaseClient
     .from('cases') // your table name
     .select('*');
 
